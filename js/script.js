65ec8ca5719aa97c7fea6622d325f810d39a6ddf
Kal-El89 <tufuria89@gmail.com>
2020-03-27 01:46:56 +0400

    header

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let burgerElement = document.getElementById('burg');
+let menuElement = document.getElementById('men');
+
+burgerElement.addEventListener('click', function(){
+burgerElement.classList.toggle("active");
+menuElement.classList.toggle("active");
+})
